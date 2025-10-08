ed3ee4c6de29f0857da39c92c963d04e5314d066
ЕгорА <@DESKTOP-KES48VQ>
2025-10-08 19:21:17 +0300

    Добавлен класс для врагов. Добавлен метод генерации противников.

--- PR1/Program.cs
+++ PR1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
 
+// базовый класс для сущностей (игрок и враги)
 abstract class Entity
 {
     public int HP { get; set; }
@@ -10,7 +11,7 @@ abstract class Entity
     public int Attack { get; set; }
     public int Defense { get; set; }
 
-    protected EntityHandle(int HP, int MaxHP, int Attack, int Defense)
+    protected Entity(int hp, int attack, int defense)
     {
         MaxHP = hp;
         HP = hp;
@@ -18,15 +19,18 @@ abstract class Entity
         Defense = defense;
     }
 
-    public abstract void AttackTarget(Entity Target, Random rand)
+    // абстрактный метод атаки
+    public abstract void AttackTarget(Entity target, Random rand);
 }
 
+// интерфейс для предметов
 interface IItem
 {
     string Name { get; }
     void Equip(Player player);
 }
 
+// оружие
 class Weapon : IItem
 {
     public string Name { get; }
@@ -38,58 +42,58 @@ class Weapon : IItem
         AttackBonus = attackBonus;
     }
 
-    public Equip(string name, int attackBonus)
+    public void Equip(Player player)
     {
         player.Weapon = this;
-        player.Attack = 5 + AttackBonus;
+        player.Attack = 5 + AttackBonus; // базовая атака + бонус
     }
 
-    public override string ToString() => $" {Name} (Атака: {AttackBonus})";
+    public override string ToString() => $"{Name} (Атака: {AttackBonus})";
 }
 
+// доспехи
 class Armor : IItem
 {
-    class Weapon : IItem
+    public string Name { get; }
+    public int DefenseBonus { get; }
+
+    public Armor(string name, int defenseBonus)
     {
-        public string Name { get; }
-        public int DefenseBonus { get; }
-
-        public Weapon(string name, int defenseBonus)
-        {
-            Name = name;
-            DefenseBonus = defenseBonus;
-        }
-
-        public Equip(string name, int defenseBonus)
-        {
-            player.Armor = this;
-            player.Defense = 5 + DefenseBonus;
-        }
-
-        public override string ToString() => $" {Name} (Защита: {DefenseBonus})";
+        Name = name;
+        DefenseBonus = defenseBonus;
     }
+
+    public void Equip(Player player)
+    {
+        player.Armor = this;
+        player.Defense = 2 + DefenseBonus; // базовая защита + бонус
+    }
+
+    public override string ToString() => $"{Name} (Защита: {DefenseBonus})";
 }
 
+// зелье исцеления
 class Potion
 {
-    public string Name => "Зелье здоровья";
+    public string Name => "Зелье исцеления";
 
     public void Use(Player player)
     {
-        player.HP =player.MaxHP
-        Console.WriteLine("Здоровье восстановлено!")
+        player.HP = player.MaxHP;
+        Console.WriteLine("Зелье исцеления! HP восстановлено!");
     }
 
     public override string ToString() => Name;
 }
 
+// игрок
 class Player : Entity
 {
     public Weapon Weapon { get; set; }
     public Armor Armor { get; set; }
     public bool IsFrozen { get; set; }
 
-    public Player() : base(100, 5, 2)
+    public Player() : base(100, 8, 3) // базовые значения
     {
         Weapon = new Weapon("Кулаки", 0);
         Armor = new Armor("Тряпье", 0);
@@ -103,12 +107,13 @@ class Player : Entity
         Console.WriteLine($"Вы наносите {damage} урона!");
     }
 
+    // защита
     public int Defend(Random rand)
     {
         if (rand.NextDouble() < 0.4)
         {
             Console.WriteLine("Вы уклонились от атаки!");
-            return -1;
+            return -1; // уклонение
         }
         else
         {
@@ -120,6 +125,7 @@ class Player : Entity
     }
 }
 
+// враг
 class Enemy : Entity
 {
     public string Name { get; }
@@ -170,3 +176,41 @@ class Enemy : Entity
     }
 }
 
+// Фабрика для генерации врагов и боссов
+class EnemyFactory
+{
+    private Random rand = new Random();
+
+    public Enemy GenerateEnemy()
+    {
+        int type = rand.Next(3);
+        switch (type)
+        {
+            case 0: return new Enemy("Гоблин", 30, 8, 2, 0.2, 0, false);
+            case 1: return new Enemy("Скелет", 40, 7, 3, 0, 0, true);
+            case 2: return new Enemy("Маг", 35, 9, 1, 0, 0.15, false);
+            default: return null;
+        }
+    }
+
+    public Enemy GenerateBoss()
+    {
+        int type = rand.Next(4);
+        switch (type)
+        {
+            case 0: return new Enemy("ВВГ", (int)(50 * 2.0), (int)(8 * 1.5), (int)(2 * 1.2), 0.2 + 0.1, 0, false);
+            case 1: return new Enemy("Ковальский", (int)(60 * 2.5), (int)(7 * 1.3), (int)(3 * 1.4), 0, 0, true);
+            case 2: return new Enemy("Архимаг C++", (int)(40 * 1.8), (int)(9 * 1.6), (int)(1 * 1.1), 0, 0.15 + 0.1, false);
+            case 3: return new Enemy("Пестов С--", (int)(60 * 1.3), (int)(7 * 1.8), (int)(3 * 0.6), 0, 0.15 + 0.15, true);
+            default: return null;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
