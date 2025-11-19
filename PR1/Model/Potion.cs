63878c6f63bbab53df1f812502f64d5e1dbe7fba
234824 <234824@KB37-118-C12>
2025-11-19 13:41:38 +0300

    Все классы и интерфейс были вынесены в отдельные файлы.

---
+++ PR1/Model/Potion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Potion
+{
+    public string Name => "Зелье исцеления";
+
+    public void Use(Player player)
+    {
+        player.HP = player.MaxHP;
+        Console.WriteLine("Зелье исцеления! HP восстановлено!");
+    }
+
+    public override string ToString() => Name;
+}
